c1db68ed997af6634c0ce56ae9d80aa635b1199c
ray3nnnn <ryansam0678@gmail.com>
2026-07-31 01:49:10 +0700

    fix railway postgres connection

--- backend/database.js
+++ backend/database.js
@@ -3,21 +3,41 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
 
-    host: process.env.PGHOST,
-
-    user: process.env.PGUSER,
-
-    password: process.env.PGPASSWORD,
-
-    database: process.env.PGDATABASE,
-
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
 
     ssl:{
         rejectUnauthorized:false
     }
 
 });
+
+
+async function testConnection(){
+
+try{
+
+await pool.query("SELECT 1");
+
+console.log("✅ PostgreSQL Connected");
+
+
+}
+catch(error){
+
+console.log(
+"Database Error:",
+error.message
+);
+
+}
+
+}
+
+
+module.exports={
+pool,
+testConnection
+};
 
 
 async function testConnection(){
